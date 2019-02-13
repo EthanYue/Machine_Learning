59ef86af662ba73701a1d4853ad24eb41031391b
EthanYue <yfy17859733505@gmail.com>
2019-02-13 21:08:37 +0800

    [新增] 贝叶斯分类和费舍尔分类

--- document_filtering/doc_class.py
+++ document_filtering/doc_class.py
@@ -10,7 +10,6 @@ def sample_train(_class):
 	_class.train('the quick brown fox jumps', 'good')
 
 
-
 def get_words(doc):
 	splitter = re.compile(r'\W')
 	words = [s.lower() for s in splitter.split(doc) if len(s) > 2 and len(s) < 20]
@@ -71,4 +70,114 @@ class classifier:
 		if self.c_count(cat) == 0:
 			return 0
 		# 特征在分类中出现的总次数，除以分类中包含内容项的总数
-		return self.f_count(f, cat)/self.c_count(cat)
+		return self.f_count(f, cat) / self.c_count(cat)
+	
+	def weighted_prob(self, f, cat, pr_func, weight=1, average_p=0.5):
+		"""
+		:param f:
+		:param cat:
+		:param pr_func: 计算概率值的函数， 当前为f_prob()
+		:param weight:
+		:param average_p: 平均初始概率为0.5
+		:return:
+		"""
+		basic_prob = pr_func(f, cat)
+		totals = sum([self.f_count(f, c) for c in self.categories()])
+		bp = ((weight * average_p) + (totals * basic_prob)) / (weight + totals)
+		return bp
+	
+	
+# 朴素贝叶斯分类
+class naive_bayes(classifier):
+	def __init__(self, get_features):
+		classifier.__init__(self, get_features)
+		self.thresholds = {}
+	
+	def set_threshold(self, cat, t):
+		self.thresholds[cat] = t
+		
+	def get_threshold(self, cat):
+		if cat not in self.thresholds:
+			return 1.0
+		return self.thresholds[cat]
+		
+	def doc_prob(self, item, cat):
+		features = self.get_features(item)
+		p = 1
+		for f in features:
+			p *= self.weighted_prob(f, cat, self.f_prob)
+		return p
+	
+	def prob(self, item, cat):
+		cat_prob = self.c_count(cat) / self.total_count()
+		doc_prob = self.doc_prob(item, cat)
+		return doc_prob * cat_prob
+	
+	def classify(self, item, default=None):
+		probs = {}
+		max = 0.0
+		best = default
+		for cat in self.categories():
+			probs[cat] = self.prob(item, cat)
+			if probs[cat] > max:
+				max = probs[cat]
+				best = cat
+		for cat in probs:
+			if cat == best:
+				continue
+			if probs[best] < probs[cat] * self.get_threshold(best):
+				return default
+		return best
+
+
+class fisher_classifier(classifier):
+	def __init__(self, get_features):
+		classifier.__init__(self, get_features)
+		self.minimums = {}
+	
+	def set_minimum(self, cat, min):
+		self.minimums[cat] = min
+	
+	def get_minimum(self, cat):
+		if cat not in self.minimums:
+			return 0
+		return self.minimums[cat]
+	
+	def c_prob(self, f, cat):
+		# 特征在该分类中出现的概率
+		cl_f = self.f_prob(f, cat)
+		if cl_f == 0:
+			return 0
+		# 特征在所有分类中出现的概率
+		freq_sum = sum([self.f_prob(f, c) for c in self.categories()])
+		p = cl_f / freq_sum
+		return p
+
+	def fisher_prob(self, item, cat):
+		p = 1
+		features = self.get_features(item)
+		# 将所有概率相乘
+		for f in features:
+			p *= self.weighted_prob(f, cat, self.c_prob)
+		# 取对数再乘-2
+		f_score = -2 * math.log(p)
+		# 利用倒置对数卡方函数求概率
+		return self.invchi2(f_score, len(features) * 2)
+	
+	def invchi2(self, chi, df):
+		m = chi / 2.0
+		sum = term = math.exp(-m)
+		for i in range(1, df // 2):
+			term *= m / i
+			sum += term
+		return min(sum, 1.0)
+	
+	def classify(self, item, default=None):
+		best = default
+		max = 0.0
+		for c in self.categories():
+			p = self.fisher_prob(item, c)
+			if p > self.get_minimum(c) and p > max:
+				best = c
+				max = p
+		return best
--- document_filtering/run.py
+++ document_filtering/run.py
@@ -1,6 +1,15 @@
 import doc_class
+# import naive_bayes
 
-_class = doc_class.classifier(doc_class.get_words)
-doc_class.sample_train(_class)
-res = _class.f_prob('quick', 'good')
+
+# naive_bayes = doc_class.naive_bayes(doc_class.get_words)
+fisher_classifier = doc_class.fisher_classifier(doc_class.get_words)
+doc_class.sample_train(fisher_classifier)
+# res = naive_bayes.classify('quick rabbit', 'unknown')
+# naive_bayes.set_threshold('bad', 3.0)
+# res = fisher_classifier.weighted_prob('quick money', 'good', fisher_classifier.c_prob)
+# res = fisher_classifier.fisher_prob('quick rabbit', 'bad')
+fisher_classifier.set_minimum('bad', 0.8)
+fisher_classifier.set_minimum('good', 0.4)
+res = fisher_classifier.classify('quick money')
 print(res)
