11098a4b712bb7c9497fa1ad52418dbba24b8f9a
EthanYue <yfy17859733505@gmail.com>
2019-02-17 11:31:38 +0800

    s

--- decision_trees/run.py
+++ decision_trees/run.py
@@ -1,11 +1,13 @@
 import tree_predict
+import zillow
 # set1, set2 = tree_predict.divide_set(tree_predict.my_data, 2, 'yes')
 # res = tree_predict.gini_impurity(tree_predict.my_data)
 # res = tree_predict.entropy(tree_predict.my_data)
 # res = tree_predict.entropy(set1)
-tree = tree_predict.build_tree(tree_predict.my_data)
+# tree = tree_predict.build_tree(tree_predict.my_data)
 # tree_predict.prune(tree, 0.1)
-tree_predict.draw_tree(tree)
+# tree_predict.draw_tree(tree)
 # res = tree_predict.classify(['(direct)', 'USA', 'yes', 5], tree)
-res = tree_predict.md_classify(['(direct)', 'USA', 'yes', None], tree)
-print(res)
+# res = tree_predict.md_classify(['(direct)', 'USA', 'yes', None], tree)
+# print(res)
+
